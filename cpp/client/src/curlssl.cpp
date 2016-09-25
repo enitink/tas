1158bd79bc037f4eadbbf0239053472a1eed2794
enitink <enitink@yahoo.com>
2016-09-25 16:15:21 +0530

    same as previous:

--- cpp/client/src/curlssl.cpp
+++ cpp/client/src/curlssl.cpp
@@ -6,17 +6,8 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
-#include <libxml/tree.h>
-#include <libxml/parser.h>
-#include <libxml/parserInternals.h>
-#include <set>
-#include <algorithm>
-#include <boost/algorithm/string.hpp>
-#include <boost/algorithm/string/erase.hpp>
 
-//curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
 using namespace std;
-using namespace boost::algorithm;
 
 size_t write_data(char *ptr, size_t size, size_t nmemb, void *userdata) {
 	std::ostringstream *stream = (std::ostringstream*)userdata;
@@ -25,89 +16,8 @@ size_t write_data(char *ptr, size_t size, size_t nmemb, void *userdata) {
 	return count;
 }
 
-bool ignore = false;
-int i = 0;
-string prevtag;
-string currtag;
-string currline;
-
-set<string> linelist;
-void filllinelist()
+bool getPage(char* page, string& output)
 {
-	string list[] = {"p", "tr", "h1", "h2", "h3", "h4", "h5", "h6", "title", "li"};
-	linelist.insert(list, list + 10);
-	set<string>::iterator it;
-	it = linelist.find(string("p"));
-	if (it != linelist.end())
-		printf("found p\n");
-}
-
-void characters_callback(void * ctx, const xmlChar * ch, int len)
-{
-	//if (ignore)
-	//	return;
-	string c((const char*)ch, len);
-	trim(c);
-	if (c.size() == 0)
-		return;
-	if (currline.size() != 0)
-		currline += " ";
-	currline += c;
-	//printf("<%s>", currtag.c_str());
-	//printf("%s\n", c.c_str());
-	//printf("</%s>\n", currtag.c_str());
-}
-
-set<string> ignorelist;// (string("p"),string("a"));
-void filluseset()
-{
-	string un[] = {"script"};
-	ignorelist.insert(un,un+1);
-}
-void start_element_callback(void *user_data, const xmlChar *name, const xmlChar **attrs) {
-	/*++i;
-	for (int j = 0; j < i; ++j)
-		printf("\t");
-	printf("start < %s > \n", name);*/
-	string n((const char*)name);
-	transform(n.begin(), n.end(), n.begin(), ::tolower);
-	/*if (ignorelist.find(n) != ignorelist.end())
-	{
-		ignore = true;
-		prevtag = n;
-	}
-	currtag = n;*/
-	if (linelist.find(n) != linelist.end())
-	{
-		currline = "";
-	//	printf("start < %s > \n", n.c_str());
-	}
-}
-
-void end_element_callback(void * ctx, const xmlChar * name)
-{
-	/*for (int j = 0; j < i; ++j)
-		printf("\t");
-	printf("end < %s >\n", name);
-	ignore = true;
-	--i;*/
-	string n((const char*)name);
-	/*if (n == prevtag)
-	{
-		ignore = false;
-		prevtag="";
-	}*/
-	if (linelist.find(n) != linelist.end())
-	{
-		printf("%s\n", currline.c_str());
-	//	printf("end < %s > \n", n.c_str());
-	}
-}
-
-int main(void)
-{
-  filluseset();
-  filllinelist();
   CURL *curl;
   CURLcode res;
 
@@ -135,34 +45,7 @@ int main(void)
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
   
-	// Initialize all fields to zero
-	xmlSAXHandler sh = { 0 };
-	// register callback
-	sh.startElement = start_element_callback;
-	sh.endElement = end_element_callback;
-	sh.characters = characters_callback;
-	xmlParserCtxtPtr ctxt;
-	string str = stream.str();
-	//erase_all(str,"<br>");
-	//erase_all(str,"</br>");
-
-	if ((ctxt = xmlCreateMemoryParserCtxt(str.c_str(), str.size())) == NULL) {
-		fprintf(stderr, "Erreur lors de la cration du contexte\n");
-		return EXIT_FAILURE;
-	}
-	// register sax handler with the context
-	ctxt->sax = &sh;
-
-	// parse the doc
-	xmlParseDocument(ctxt);
-	// well-formed document?
-	if (ctxt->wellFormed) {
-		printf("XML Document is well formed\n");
-	} else {
-		fprintf(stderr, "XML Document isn't well formed\n");
-		//xmlFreeParserCtxt(ctxt);
-		return EXIT_FAILURE;
-	}
+	output = stream.str();
 	//cout << "*******************************************************************************" << endl;
 	//cout << str.c_str();
     curl_easy_cleanup(curl);
@@ -170,5 +53,5 @@ int main(void)
 
   curl_global_cleanup();
 
-  return 0;
+  return true;
 }
--- cpp/server/src/main.cpp
+++ cpp/server/src/main.cpp
@@ -13,31 +13,47 @@
 #include <asio.hpp>
 #include "server.hpp"
 
+using namespace std;
+bool getPage(char*, string&); //defined in ../../client/src/curlssl.cpp
+bool prepareLineDocs(string&); //defined in  ../../dsalog/src/parser.cpp
+vector<string>& getParsedLineDocVector(); //defined in  ../../dsalog/src/parser.cpp
+
 int main(int argc, char* argv[])
 {
-  try
-  {
-    // Check command line arguments.
-    if (argc != 4)
-    {
-      std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    receiver 0.0.0.0 80 .\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    receiver 0::0 80 .\n";
-      return 1;
-    }
+	string htmlpage;
+	if (getPage("http://en.wikipedia.org/wiki/Filmfare_Award_for_Best_Actor", htmlpage) == false)
+	{
+		cerr << "**Failed to get the page : http://en.wikipedia.org/wiki/Filmfare_Award_for_Best_Actor";
+		exit(0);
+	}
+	prepareLineDocs(htmlpage);
+	vector<string>& vec = getParsedLineDocVector();
 
-    // Initialise the server.
-    http::server::server s(argv[1], argv[2], argv[3]);
+	for (vector<string>::iterator it = vec.begin(); it != vec.end(); ++it)
+		cout << *it << endl; 
+	try
+	{
+		// Check command line arguments.
+		if (argc != 4)
+		{
+			cerr << "Usage: http_server <address> <port> <doc_root>\n";
+			cerr << "  For IPv4, try:\n";
+			cerr << "    receiver 0.0.0.0 80 .\n";
+			cerr << "  For IPv6, try:\n";
+			cerr << "    receiver 0::0 80 .\n";
+			return 1;
+		}
 
-    // Run the server until stopped.
-    s.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "exception: " << e.what() << "\n";
-  }
+		// Initialise the server.
+		http::server::server s(argv[1], argv[2], argv[3]);
 
-  return 0;
+		// Run the server until stopped.
+		s.run();
+	}
+	catch (exception& e)
+	{
+		cerr << "exception: " << e.what() << "\n";
+	}
+
+	return 0;
 }
